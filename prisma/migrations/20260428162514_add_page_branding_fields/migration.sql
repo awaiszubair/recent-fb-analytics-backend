3b33f25832b7e51f73856eb4d9671a045f24e36c
awaiszubair <awaiszubair417@gmail.com>
2026-04-28 21:29:05 +0500

    Prisma conflict resolved

--- prisma/migrations/20260428162514_add_page_branding_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "connected_pages" ADD COLUMN     "category" TEXT,
-ADD COLUMN     "picture_url" TEXT;
-
--- AlterTable
-ALTER TABLE "posts" ADD COLUMN     "comments_count" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "full_picture" TEXT,
-ADD COLUMN     "shares_count" INTEGER NOT NULL DEFAULT 0;
